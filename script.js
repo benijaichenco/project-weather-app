94de35041f4198654f7987939d8ac59e6219eb7d
Beni <benjaminjaichenco@gmail.com>
2023-09-01 20:48:55 +0300

    generate async function getWeather

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const url =
+  "http://api.weatherapi.com/v1/forecast.json?key=5d3dabed1d754a5e999103359232408&days=7&aqi=no&alerts=no&q=";
+
+let unitChoice = "metric";
+let place = "tel aviv";
+
+async function getWeather() {
+  const response = await fetch(url + place);
+  const json = await response.json();
+  console.log(json);
+  return json;
+}
+
+getWeather();
