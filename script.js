32a334cf3474f385561f6538ca5276da5db0622f
Beni <benjaminjaichenco@gmail.com>
2023-09-01 21:12:23 +0300

    generate save data function

--- script.js
+++ script.js
@@ -7,45 +7,38 @@ let place = "tel aviv";
 let info = {
   forecast: [
     {
-      day: "",
       degree: "",
-      unit: "",
+      conditionText: "",
       conditionIcon: "",
     },
     {
       day: "",
       degree: "",
-      unit: "",
       conditionIcon: "",
     },
     {
       day: "",
       degree: "",
-      unit: "",
       conditionIcon: "",
     },
     {
       day: "",
       degree: "",
-      unit: "",
-      conditionIcon: "",
-    },
-    {
-      dday: "",
-      degree: "",
-      unit: "",
       conditionIcon: "",
     },
     {
       day: "",
       degree: "",
-      unit: "",
       conditionIcon: "",
     },
     {
       day: "",
       degree: "",
-      unit: "",
+      conditionIcon: "",
+    },
+    {
+      day: "",
+      degree: "",
       conditionIcon: "",
     },
   ],
@@ -54,8 +47,30 @@ let info = {
 async function getWeather() {
   const response = await fetch(url + place);
   const json = await response.json();
-  console.log(json);
-  return json;
+  saveData(json);
 }
+
+function saveData(data) {
+  if (unitChoice === "metric") {
+    info.forecast[0].degree = data.current.temp_c;
+  } else if (unitChoice === "imperial") {
+    info.forecast[0].degree = data.current.temp_f;
+  }
+  info.forecast[0].conditionIcon = data.current.condition.icon;
+  info.forecast[0].conditionText = data.current.condition.text;
+  for (let i = 1; i < info.forecast.length; i++) {
+    info.forecast[i].day = dayInWeek(data.forecast.forecastday[i].date);
+    if (unitChoice === "metric") {
+      info.forecast[i].degree = data.forecast.forecastday[i].day.avgtemp_c;
+    } else if (unitChoice === "imperial") {
+      info.forecast[i].degree = data.forecast.forecastday[i].day.avgtemp_f;
+    }
+    info.forecast[i].conditionIcon =
+      data.forecast.forecastday[i].day.condition.icon;
+  }
+  console.log(data);
+}
+
+function dayInWeek(date) {}
 
 getWeather();
